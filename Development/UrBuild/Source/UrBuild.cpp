37d678fc7763417faeeb4eeab254e6db9a5aad76
Arelius <Arelius@Gmail.com>
2007-11-28 03:49:08 +0000

    Added missingUrBuild.cpp to workspace.

---
+++ Development/UrBuild/Source/UrBuild.cpp
@@ -0,0 +1,11 @@
+// UrBuild.cpp : Defines the entry point for the console application.
+//
+#include "Core.h"
+#include "UBuildScript.h"
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	RunBuildScripts();
+	return 0;
+}
+
